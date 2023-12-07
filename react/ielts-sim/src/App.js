ea747d6eada49b6c2482d21ccae08e47fa01660b
zGadli <kartikgadli303@gmail.com>
2023-12-07 17:46:26 +0530

    functional component test

--- react/ielts-sim/src/App.js
+++ react/ielts-sim/src/App.js
@@ -2,11 +2,20 @@ import './App.css';
 import Greet from './components/Greet'
 
 
-function App() {
+const App = () => {
+
+  const name = 'React';
+  const Person = () => {
+    return <>
+      <h2>First Name: Kartik</h2>
+      <h2>Last Name: Gadagalli</h2>
+      <h2>Age: 22</h2>
+    </>
+  }
   return (
     <div className="App">
       <Greet />
-
+      <Person />
     </div>
   );
 }
